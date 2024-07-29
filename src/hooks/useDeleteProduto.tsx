a5e1eb8e52c0140d98e295328d167b818933fbf3
thais ventura <thaisvtr26@gmail.com>
2024-07-28 22:24:38 -0300

    :sparkles:feat: hook Delete Produto

---
+++ src/hooks/useDeleteProduto.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const useDeleteProduto = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const deleteProduto = async (id: number) => {
+    setLoading(true);
+    setError(null);
+    try {
+      await axios.delete(`http://localhost:5000/api/produtos/${id}`);
+      setLoading(false);
+      return true;
+    } catch (error) {
+      setError('Erro ao excluir o produto. Por favor, tente novamente.');
+      setLoading(false);
+      return false;
+    }
+  };
+
+  return { deleteProduto, loading, error };
+};
+
+export default useDeleteProduto;
