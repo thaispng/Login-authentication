6afea20cdf0b597bdc60d5bc00d6d4b391325868
thais ventura <thaisvtr26@gmail.com>
2024-07-28 22:23:39 -0300

    :sparkles:feat: SideMenu

--- src/app/Components/_ui/SideMenu.tsx
+++ src/app/Components/_ui/SideMenu.tsx
@@ -1,54 +1,43 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import Image from 'next/image';
 import { House, Box, Boxes, FileBox, Users, GraduationCap, ArrowLeft, LogOut } from 'lucide-react';
+import clsx from 'clsx';
 
-const SideMenu: React.FC = () => {
+const SideMenu = () => {
   const [isOpen, setIsOpen] = useState(true);
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(prev => !prev);
   };
 
+  const menuItems = useMemo(() => [
+    { href: '#', icon: House, label: 'Home' },
+    { href: '#', icon: Box, label: 'Estoque' },
+    { href: '#', icon: Boxes, label: 'Categoria' },
+    { href: '#', icon: GraduationCap, label: 'Escolas' },
+    { href: '#', icon: FileBox, label: 'Gestão de pedidos' },
+    { href: '#', icon: Users, label: 'Usuários' },
+    { href: '#', icon: LogOut, label: 'Sair' },
+  ], []);
+
   return (
-    <div className={`fixed inset-y-0 left-0 ${isOpen ? 'w-64' : 'w-20'} bg-container-white text-white z-40 transition-all duration-300`}>
-      <div className="flex items-center justify-between px-4 py-4">
+    <div className={`inset-y-0 left-0 ${isOpen ? 'w-64' : 'w-20'} bg-container-white text-white z-40 transition-width duration-300 select-none whitespace-nowrap`}>
+      <div className="flex items-center justify-between px-3 py-4">
         <Image src="/Logotrajetonb 2.svg" width={50} height={50} alt="Logo" />
       </div>
       <nav className="flex flex-col gap-5 mt-4 p-2">
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <House className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Home</span>}
-        </a>
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <Box className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Estoque</span>}
-        </a>
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <Boxes className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Categoria</span>}
-        </a>
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <GraduationCap className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Escolas</span>}
-        </a>
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <FileBox className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Gestão de pedidos</span>}
-        </a>
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <Users className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Usuários</span>}
-        </a>
-        <a onClick={toggleMenu} className="cursor-pointer flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <ArrowLeft className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
+        {menuItems.map(({ href, icon: Icon, label }) => (
+          <a key={label} href={href} className="flex items-center px-3 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
+            <Icon className="mr-2 text-base transition-all flex-none" />
+            <span className={clsx('transition-all', isOpen ? 'opacity-100': 'opacity-0')}>{label}</span>
+          </a>
+        ))}
+        <span onClick={toggleMenu} className="cursor-pointer flex items-center px-3 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
+          <ArrowLeft className="mr-2 text-base" />
           {isOpen && <span>Recolher</span>}
-        </a>
-        <a href="#" className="flex items-center px-4 py-2 text-base text-gray-600 font-medium hover:bg-gray-100 rounded-md focus:text-orange-600 focus:bg-gray-100">
-          <LogOut className={`mr-2 ${isOpen ? 'text-base' : 'text-xl'}`} />
-          {isOpen && <span>Sair</span>}
-        </a>
+        </span>
       </nav>
     </div>
   );
